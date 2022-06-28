7dd44f7ce461714ffed59c69fca8cd5b5e1b8a6e
Alexander Stenberg <alstenberg@live.se>
2022-06-28 20:26:03 +0200

    added product cards and info section

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ProductCard(props) {
   return (
     <main className="card-container">
-      <h2 className="card-title__head">{props.titlehead}</h2>
-      <p className="card-desc__head">{props.deschead}</p>
+      {/* <h2 className="card-title__head">{props.titlehead}</h2>
+      <p className="card-desc__head">{props.deschead}</p> */}
       <section className="card-content">
         <img className="card-image" alt="card" src={props.img} />
         <h3 className="card-title">{props.title}</h3>
--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -7,30 +7,33 @@ import studio from '../img/nvidia-studio.png';
 
 export default function ProductInfo() {
   return (
-    <div>
-      <ProductCard
-        titlehead={'KÖP'}
-        img={laptop}
-        title={'NVIDIA rekommenderar'}
-        desc={'De bästa erbjudandena på bärbara datorer. Köp nu'}
-      />
-      <ProductCard
-        img={graphics}
-        title={'Grafikkort i GeForce RTX-serien'}
-        desc={'Nu är NVIDIA GeForce RTX 30-serien tillbaka i lager!'}
-      />
-      <ProductCard
-        img={geforce}
-        title={'GFN Thursday'}
-        desc={'Se vad du kan streama på GeForce NOW den här veckan'}
-      />
-      <ProductCard
-        img={studio}
-        title={'I NVIDIA STUDIO'}
-        desc={
-          'Vi firar fantastiska konstnärer, inspirerande konst och publicerar nyheter för innehållsskapare varje vecka.'
-        }
-      />
-    </div>
+    <main className="info-container">
+      <h2>KÖP</h2>
+      <section className="info-cards">
+        <ProductCard
+          titlehead={'KÖP'}
+          img={laptop}
+          title={'NVIDIA rekommenderar'}
+          desc={'De bästa erbjudandena på bärbara datorer. Köp nu'}
+        />
+        <ProductCard
+          img={graphics}
+          title={'Grafikkort i GeForce RTX-serien'}
+          desc={'Nu är NVIDIA GeForce RTX 30-serien tillbaka i lager!'}
+        />
+        <ProductCard
+          img={geforce}
+          title={'GFN Thursday'}
+          desc={'Se vad du kan streama på GeForce NOW den här veckan'}
+        />
+        <ProductCard
+          img={studio}
+          title={'I NVIDIA STUDIO'}
+          desc={
+            'Vi firar fantastiska konstnärer, inspirerande konst och publicerar nyheter för innehållsskapare varje vecka.'
+          }
+        />
+      </section>
+    </main>
   );
 }
